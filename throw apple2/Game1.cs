dcb566ea4b16fd607b1f194a107c1190754a4d86
Jesper Persson <runneberg@live.se>
2022-01-18 10:46:01 +0100

    player collision funkar lite

--- throw apple2/Game1.cs
+++ throw apple2/Game1.cs
@@ -43,51 +43,13 @@ namespace throw_apple2 {
 
         protected override void Update(GameTime gameTime) {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))Exit();
-            KeyboardState _ks = Keyboard.GetState();
-            //forward
-            if (_ks.IsKeyDown(Keys.W)) {
-                _player1.Forward = true;
-            } else {
-                _player1.Forward = false;
-            }
-            //backwards
-            if (_ks.IsKeyDown(Keys.S)) {
-                _player1.Back = true;
-            } else {
-                _player1.Back = false;
-            }
-            //rotation
-            if (_ks.IsKeyDown(Keys.A)) {
-                _player1.Rotation -= 0.1f;
-            } else if (_ks.IsKeyDown(Keys.D)) {
-                _player1.Rotation += 0.1f;
-            }
+            
 
-            //player2
-            if (_ks.IsKeyDown(Keys.Up)) {
-                _player2.Forward = true;
-            }
-            else {
-                _player2.Forward = false;
-            }
-            //backwards
-            if (_ks.IsKeyDown(Keys.Down)) {
-                Debug.WriteLine("hej");
-                _player2.Back = true;
-            }else {
-                _player2.Back = false;
-            }
-            //rotation
-            if (_ks.IsKeyDown(Keys.Left)) {
-                _player2.Rotation -= 0.1f;
-            }
-            else if (_ks.IsKeyDown(Keys.Right)) {
-                _player2.Rotation += 0.1f;
-            }
-
-           
+            move(_player1, _player2);
             collision(_wall1, _player1);
             collision(_wall1, _player2);
+            playerCollision(_player2, _player1);
+            playerCollision(_player1, _player2);
             worldBounds(_player1);
             worldBounds(_player2);
             _player1.Update();
@@ -104,8 +66,53 @@ namespace throw_apple2 {
             _spriteBatch.End();
             base.Draw(gameTime);
         }
-        
 
+        private void move(Player a, Player b)
+        {
+            KeyboardState _ks = Keyboard.GetState();
+            if (_ks.IsKeyDown(Keys.W)) {
+                a.Forward = true;
+            }
+            else {
+                a.Forward = false;
+            }
+            //backwards
+            if (_ks.IsKeyDown(Keys.S)) {
+                a.Back = true;
+            }
+            else {
+                a.Back = false;
+            }
+            //rotation
+            if (_ks.IsKeyDown(Keys.A)) {
+                a.Rotation -= 0.1f;
+            }
+            else if (_ks.IsKeyDown(Keys.D)) {
+                a.Rotation += 0.1f;
+            }
+
+            //player2
+            if (_ks.IsKeyDown(Keys.Up)) {
+                b.Forward = true;
+            }
+            else {
+                b.Forward = false;
+            }
+            //backwards
+            if (_ks.IsKeyDown(Keys.Down)) {
+                b.Back = true;
+            }
+            else {
+                b.Back = false;
+            }
+            //rotation
+            if (_ks.IsKeyDown(Keys.Left)) {
+                b.Rotation -= 0.1f;
+            }
+            else if (_ks.IsKeyDown(Keys.Right)) {
+                b.Rotation += 0.1f;
+            }
+        }
         private void worldBounds(Entity e) {
             if (e.Position.X - e.HalfWidth < 0) {
                 e.Position = new Vector2(e.HalfWidth, e.Position.Y);
@@ -127,19 +134,67 @@ namespace throw_apple2 {
                 double angle = a.playerAngle(b);
                 double cornerAngle = a.cornerAngle();
 
+                //krockar från ovansidan
                 if (angle > cornerAngle && angle < Math.PI - cornerAngle) {
                     b.Position = new Vector2(b.Position.X, a.Position.Y - a.HalfHeight - b.HalfHeight);
-
                 }
+                //krokar från vänster sida
                 else if (angle > Math.PI - cornerAngle || angle < cornerAngle - Math.PI) {
                     b.Position = new Vector2(a.Position.X - a.HalfWidth - b.HalfWidth, b.Position.Y);
                 }
+                //krockar från nedansidan
                 else if (angle < -cornerAngle && angle >  cornerAngle - Math.PI) {
                     b.Position = new Vector2(b.Position.X, a.Position.Y + a.HalfHeight + b.HalfHeight);
                 }
+                //krockar från höger sida
                 else if (angle < cornerAngle && angle > -cornerAngle) {
                     b.Position = new Vector2(a.Position.X + a.HalfWidth + b.HalfWidth, b.Position.Y);
                 }
+            }
+        }
+        private void playerCollision(Player p1, Player p2)
+        {
+            Vector2 delta = p1.Position - p2.Position;
+            float distance = MathF.Sqrt(delta.X * delta.X + delta.Y * delta.Y);
+            if (distance < p1.HalfHeight + p2.HalfHeight) {
+                double angle = p1.playerAngle(p2);
+                double cornerAngle = Math.PI/4;
+                if (p1.Velocity.X == 0 || p1.Velocity.Y == 0) {
+                    //krockar från ovansidan
+                    if (angle > cornerAngle && angle < Math.PI - cornerAngle) {
+                        p2.Position = new Vector2(p2.Position.X, p1.Position.Y - p1.HalfHeight - p2.HalfHeight);
+                    }
+                    //krokar från vänster sida
+                    else if (angle > Math.PI - cornerAngle || angle < cornerAngle - Math.PI) {
+                        p2.Position = new Vector2(p1.Position.X - p1.HalfWidth - p2.HalfWidth, p2.Position.Y);
+                    }
+                    //krockar från nedansidan
+                    else if (angle < -cornerAngle && angle >  cornerAngle - Math.PI) {
+                        p2.Position = new Vector2(p2.Position.X, p1.Position.Y + p1.HalfHeight + p2.HalfHeight);
+                    }
+                    //krockar från höger sida
+                    else if (angle < cornerAngle && angle > -cornerAngle) {
+                        p2.Position = new Vector2(p1.Position.X + p1.HalfWidth + p2.HalfWidth, p2.Position.Y);
+                    }
+                } else if(p2.Velocity.X == 0 || p2.Velocity.Y == 0) {
+                    //krockar från ovansidan
+                    if (angle > cornerAngle && angle < Math.PI - cornerAngle) {
+                        p1.Position = new Vector2(p1.Position.X, p2.Position.Y - p2.HalfHeight - p1.HalfHeight);
+                    }
+                    //krokar från vänster sida
+                    else if (angle > Math.PI - cornerAngle || angle < cornerAngle - Math.PI) {
+                        p1.Position = new Vector2(p2.Position.X - p2.HalfWidth - p1.HalfWidth, p1.Position.Y);
+                    }
+                    //krockar från nedansidan
+                    else if (angle < -cornerAngle && angle >  cornerAngle - Math.PI) {
+                        p1.Position = new Vector2(p1.Position.X, p2.Position.Y + p2.HalfHeight + p1.HalfHeight);
+                    }
+                    //krockar från höger sida
+                    else if (angle < cornerAngle && angle > -cornerAngle) {
+                        p1.Position = new Vector2(p2.Position.X + p2.HalfWidth + p1.HalfWidth, p1.Position.Y);
+                    }
+                }
+
             }
         }
     }
--- throw apple2/Player.cs
+++ throw apple2/Player.cs
@@ -5,6 +5,7 @@ namespace throw_apple2 {
     class Player : Entity {
         private bool _forward;
         private bool _back;
+        private bool _Playercollision;
         private float _speed = 0.4f;
 
         public Player(Game game, string texturename, Vector2 position) : base(game, texturename, position) {
@@ -32,11 +33,14 @@ namespace throw_apple2 {
             HalfHeight = s;
             HalfWidth = s;
         }
-        /*public override Rectangle getRectangle() {
-            HalfHeight = 20;
-            HalfWidth = 20;
-            return new Rectangle(_position.ToPoint(), new Point(20, 20));
-        }*/
+        public double playerAngle(Player a)
+        {
+            return Math.Atan2(Position.Y - a.Position.Y, a.Position.X - Position.X);
+        }
+        public double cornerAngle()
+        {
+            return Math.Atan2(HalfHeight, HalfWidth);
+        }
         public bool Forward { get => _forward; set => _forward = value; }
         public float Speed { get => _speed; set => _speed = value; }
         public bool Back { get => _back; set => _back = value; }
